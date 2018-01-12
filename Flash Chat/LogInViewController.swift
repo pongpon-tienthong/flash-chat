217f392a678de9da375ed720290909d1274f52b7
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2018-01-12 14:32:26 -0600

    implement Log in functionality

--- Flash Chat/LogInViewController.swift
+++ Flash Chat/LogInViewController.swift
@@ -6,7 +6,7 @@
 
 
 import UIKit
-
+import Firebase
 
 class LogInViewController: UIViewController {
 
@@ -26,9 +26,17 @@ class LogInViewController: UIViewController {
    
     @IBAction func logInPressed(_ sender: AnyObject) {
 
-        
         //TODO: Log in the user
-        
+        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+            
+            if error != nil {
+                print(error!)
+            }
+            else {
+                print("Log in successful")
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
         
     }
     
