eb57b4334ad20453f5bf2a92e07a76d927bdc93e
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2018-01-12 15:31:27 -0600

    Add animation when editing text field

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     
     // Declare instance variables here
 
@@ -30,17 +30,15 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         messageTableView.dataSource = self
         
         //TODO: Set yourself as the delegate of the text field here:
-
-        
+        messageTextfield.delegate = self
         
         //TODO: Set the tapGesture here:
-        
-        
-
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector (tableViewTapped))
+        messageTableView.addGestureRecognizer(tapGesture)
+    
         //TODO: Register your MessageCell.xib file here:
         messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
         configureTableView()
-        
     }
 
     ///////////////////////////////////////////
@@ -58,16 +56,15 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         return cell
     }
     
-    
     //TODO: Declare numberOfRowsInSection here:
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
     
-    
     //TODO: Declare tableViewTapped here:
-    
-    
+    @objc func tableViewTapped() {
+        messageTextfield.endEditing(true)
+    }
     
     //TODO: Declare configureTableView here:
     func configureTableView() {
@@ -84,12 +81,20 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
 
     
     //TODO: Declare textFieldDidBeginEditing here:
-    
-    
-    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.5) {
+            self.heightConstraint.constant = 308
+            self.view.layoutIfNeeded()
+        }
+    }
     
     //TODO: Declare textFieldDidEndEditing here:
-    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.5) {
+            self.heightConstraint.constant = 50
+            self.view.layoutIfNeeded()
+        }
+    }
 
     
     ///////////////////////////////////////////
